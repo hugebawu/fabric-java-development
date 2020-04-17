9e70b26a56e62dd810c61ce2062ed74b358bf125
hugebawu <drbjhu@163.com>
2020-04-17 22:51:36 +0800

    update c4 fetch-config-block

--- c4/standard-channel-config/fetch-config-block.sh
+++ c4/standard-channel-config/fetch-config-block.sh
@@ -7,9 +7,9 @@ if [ ! -d ${ARTIFACT} ];then
 fi
 
 echo fetch system config...
-CORE_PEER_MSPCONFIGPATH=./crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp \
+CORE_PEER_MSPCONFIGPATH=./crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp \
 CORE_PEER_LOCALMSPID=Org1MSP \
-peer channel fetch config ${ARTIFACT}/config-block.pb -o localhost:7050 -c system-channel
+peer channel fetch config ${ARTIFACT}/config-block.pb -o localhost:7050 -c ch1
 
 configtxlator proto_decode --input ${ARTIFACT}/config-block.pb --type common.Block --output ${ARTIFACT}/config-block.json
 
