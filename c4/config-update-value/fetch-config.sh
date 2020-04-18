7da01c3b3b0cfd4858e6ea3fbd3e39b2ecfb5695
hugebawu <drbjhu@163.com>
2020-04-18 17:03:03 +0800

    c4/config-update-value/network/artifact/*

--- c4/config-update-value/fetch-config.sh
+++ c4/config-update-value/fetch-config.sh
@@ -9,7 +9,7 @@ fi
 FABRIC_CFG_PATH=./network \
 CORE_PEER_MSPCONFIGPATH=crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp \
 CORE_PEER_LOCALMSPID=Org1MSP \
-peer channel fetch config ${ARTIFACT}/config-block.pb -c ch1
+peer channel fetch config ${ARTIFACT}/config-block.pb -o localhost:7050 -c ch1
 
 configtxlator proto_decode --input ${ARTIFACT}/config-block.pb --type common.Block --output ${ARTIFACT}/config-block.json
 jq .data.data[0].payload.data.config ${ARTIFACT}/config-block.json > ${ARTIFACT}/config.json
